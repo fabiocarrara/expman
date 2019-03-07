1514bdc97d034161a8ccfedd1798834f80ca75b9
Fabio Carrara <fabio.carrara@isti.cnr.it>
2019-03-07 16:11:39 +0100

    added __init__ with top-level functions

---
+++ expman/__init__.py
@@ -0,0 +1,8 @@
+from .experiment import Experiment
+
+abbreviate = Experiment.abbreviate
+collect_all = Experiment.collect_all
+filter = Experiment.filter
+from_dir = Experiment.from_dir
+gather = Experiment.gather
+is_exp_dir = Experiment.is_exp_dir
