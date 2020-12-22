923172a67e62696e68ed0d690fef10c6a8010079
Fabio Carrara <fabio.carrara@isti.cnr.it>
2020-12-22 18:10:01 +0100

    added mandatory metadata (url, author, author_email)

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name='ExpMan',
+    name='expman',
+    description='a simple experiment manager and organizer',
     version='0.1',
+    url='https://github.com/fabiocarrara/expman',
+    author='Fabio Carrara',
+    author_email='fabio.carrara@isti.cnr.it',
     packages=['expman']
 )
